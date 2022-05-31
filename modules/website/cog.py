c789472398b05e0461ceb0998031ad31abc6d085
Niclas <niclas.arnoldner@gmail.com>
2022-05-31 19:16:28 +0200

    Added website command

---
+++ modules/website/cog.py
@@ -0,0 +1,25 @@
+from discord.ext import commands
+import discord
+import discord_slash
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_choice, create_option
+from discord.ext.tasks import loop
+from discord_slash import cog_ext
+import datetime
+
+
+class website(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+
+    @commands.command(name='website')
+    @commands.guild_only()
+    async def website(self, ctx):
+            await ctx.send("https://www.lauchschwert.xyz")
+
+
+
+
+def setup(bot):
+    bot.add_cog(website(bot))
