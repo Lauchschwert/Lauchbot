f5d21c1a2e5499bec904990a177dfcf1efd2eab5
anditv21 <75949923+anditv21@users.noreply.github.com>
2022-06-02 17:10:18 +0200

    Added ban command

---
+++ modules/ban/cog.py
@@ -0,0 +1,31 @@
+from discord.ext import commands
+import discord
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_choice, create_option
+from discord.ext.tasks import loop
+import pip._vendor.requests
+
+
+
+class ban(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+
+    @commands.command(name='ban')
+    async def ban(self, ctx, member:discord.User=None, reason =None):
+            if member == None or member == ctx.message.author:
+                await ctx.channel.send("You cannot ban yourself")
+                return
+            if reason == None:
+                reason = "no reason provided"
+            message = f"You have been banned from {ctx.guild.name} for {reason}"
+            await ctx.guild.ban(member, reason=reason)
+            await member.send(message)
+            await ctx.channel.send("Ban 🔨 has spoken")
+            await ctx.channel.send(f"{member} is now banned!")
+
+
+        
+def setup(bot):
+    bot.add_cog(ban(bot))
