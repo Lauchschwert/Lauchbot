6aff3078205df2349ce3fb483e52aec4a09053ae
anditv21 <75949923+anditv21@users.noreply.github.com>
2022-08-09 23:32:34 +0200

    🔧 Improved YT-Download command

--- modules/download/cog.py
+++ modules/download/cog.py
@@ -18,17 +18,33 @@ class download(commands.Cog):
 
     @commands.command(name='download')
     async def download(self, ctx, url):
-        vgm_url = 'https://8downloader.com/download?v=' + url
-        html_text = pip._vendor.requests.get(vgm_url).text
-        soup = BeautifulSoup(html_text, 'html.parser')
-        download = soup.find('a', href=True, text='Download')['href']
-        print(str(download))
+        try:
+            if "https://youtu.be/" in url:
+                url.replace("https://youtu.be/",
+                            "https://www.youtube.com/watch?v=")
 
-        link = "http://tinyurl.com/api-create.php?url=" + str(download)
-        from urllib.request import urlopen
-        with urlopen(link) as webpage:
-            f = webpage.read().decode()
-        await ctx.send(f"Download Link to your Youtube Video (limited time only): {f}")
+            vgm_url = 'https://8downloader.com/download?v=' + url
+            html_text = pip._vendor.requests.get(vgm_url).text
+            soup = BeautifulSoup(html_text, 'html.parser')
+            download = soup.find('a', href=True, text='Download')['href']
+
+            link = "http://tinyurl.com/api-create.php?url=" + str(download)
+            from urllib.request import urlopen
+            with urlopen(link) as webpage:
+                f = webpage.read().decode()
+                
+            embed=discord.Embed(title="Click here to download your Video", url=f, color=0xff0000)
+            embed.set_author(name="Your download link is ready")
+            await ctx.reply(embed=embed)
+        except Exception as e:
+            embed = discord.Embed(title=":x: Command Error",
+            colour=0x992D22)  # Dark Red
+            embed.add_field(name="Error", value=e)
+            embed.add_field(name="Guild", value=ctx.guild)
+            embed.add_field(name="Channel", value=ctx.channel)
+            embed.add_field(name="User", value=ctx.author)
+            embed.timestamp = datetime.datetime.utcnow()
+            await ctx.reply(embed=embed)
 
 
 def setup(bot):
