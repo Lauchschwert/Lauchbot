3eba9210eb9b6f240cc00d46d2bbc70bb4cd5235
Andreas <andi@anditv.dev>
2023-08-02 21:24:00 +0200

    Updated userinfo command
    NOT TESTED YET

--- modules/userinfo/cog.py
+++ modules/userinfo/cog.py
@@ -15,28 +15,19 @@ class userinfo(commands.Cog):
         
 
     @commands.command(name='userinfo')
-    async def userinfo(self, ctx, user: discord.Member):
-        try:
-            embed = discord.Embed(title=f"{user.name}'s Info", color=0x00ff00)
-            embed.add_field(name="Name", value=user.name, inline=True)
-            embed.add_field(name="ID", value=user.id, inline=True)
-            embed.add_field(name="Highest role", value=user.top_role)
-            embed.add_field(name="Joined At", value=user.joined_at.strftime("%A, %B %d %Y @ %H:%M:%S %p"))
-            embed.add_field(name="Account created on", value=user.created_at.strftime("&A, %B &d %Y @ %H:%M:&S %p"))
-            embed.set_thumbnail(url=user.avatar_url)
+    async def userinfo(self, interaction: discord.Interaction, user: discord.Member):
+        embed = discord.Embed(title=f"{user.name}'s Info", color=0x00ff00)
+        embed.add_field(name="Name", value=user.name, inline=True)
+        embed.add_field(name="ID", value=user.id, inline=True)
+        embed.add_field(name="Highest role", value=user.top_role)
+        embed.add_field(name="Joined At", value=user.joined_at.strftime("%A, %B %d %Y @ %H:%M:%S %p"))
+        embed.add_field(name="Account created on", value=user.created_at.strftime("&A, %B &d %Y @ %H:%M:&S %p"))
+        embed.set_thumbnail(url=user.avatar_url)
 
 
-            await ctx.reply(embed=embed, mention_author=False)
-        
-        except Exception as e:
-            embed = discord.Embed(title=":x: Command Error",
-            colour=0x992D22)  # Dark Red
-            embed.add_field(name="Error", value=e)
-            embed.add_field(name="Guild", value=ctx.guild)
-            embed.add_field(name="Channel", value=ctx.channel)
-            embed.add_field(name="User", value=ctx.author)
-            embed.timestamp = datetime.datetime.utcnow()
-            await ctx.reply(embed=embed)       
+        await interaction.response.send_message(embed=embed, mention_author=False)
+    
+
 
 
 
