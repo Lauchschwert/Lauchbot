d0dd6003ce5bd4e027541735fea63f559f2e393f
Andreas <andi@anditv.dev>
2023-08-02 21:21:53 +0200

    Updated rpc command

--- modules/rpc/cog.py
+++ modules/rpc/cog.py
@@ -16,34 +16,26 @@ class rpc(commands.Cog):
         
 
     @commands.command(name='rpc')
-    @commands.guild_only()
-    @has_permissions(administrator=True)
-    async def rpc(self, ctx, activity, name):
-        try:
-            if activity == 'p' or activity == 'P':
-                game = discord.Game(name)
-                await self.bot.change_presence(activity=game)
-                await self.bot.change_presence(status=discord.Status.dnd)
-                await ctx.reply("Updated Discord RPC to playing " + name, mention_author=False)
-            if activity == 'l' or activity == 'L':
-                await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=name))
-                await self.bot.change_presence(status=discord.Status.dnd)
-                await ctx.reply("Updated Discord RPC to listening to " + name, mention_author=False)
-            if activity == 'w' or activity == 'W':
-                await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=name))
-                await self.bot.change_presence(status=discord.Status.dnd)
-                await ctx.reply("Updated Discord RPC to watching " + name, mention_author=False)
+    async def rpc(self, interaction: discord.Interaction, activity, name):
+        if not interaction.user.guild_permissions.manage_webhooks:
+            await interaction.response.send_message("You do not have permission to use this command.", ephemeral=True)
+            return
+        if activity == 'p' or activity == 'P':
+            game = discord.Game(name)
+            await self.bot.change_presence(activity=game)
+            await self.bot.change_presence(status=discord.Status.dnd)
+            await interaction.response.send_message("Updated Discord RPC to playing " + name, mention_author=False)
+        if activity == 'l' or activity == 'L':
+            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=name))
+            await self.bot.change_presence(status=discord.Status.dnd)
+            await interaction.response.send_message("Updated Discord RPC to listening to " + name, mention_author=False)
+        if activity == 'w' or activity == 'W':
+            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=name))
+            await self.bot.change_presence(status=discord.Status.dnd)
+            await interaction.response.send_message("Updated Discord RPC to watching " + name, mention_author=False)
 
 
-        except Exception as e:
-            embed = discord.Embed(title=":x: Command Error",
-            colour=0x992D22)  # Dark Red
-            embed.add_field(name="Error", value=e)
-            embed.add_field(name="Guild", value=ctx.guild)
-            embed.add_field(name="Channel", value=ctx.channel)
-            embed.add_field(name="User", value=ctx.author)
-            embed.timestamp = datetime.datetime.utcnow()
-            await ctx.reply(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(rpc(bot))
