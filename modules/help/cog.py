89bcb6547659183d959e3454fb1ec3f9d0b2368a
Andreas <andi@anditv.dev>
2023-08-02 21:13:25 +0200

    Updated help command

--- modules/help/cog.py
+++ modules/help/cog.py
@@ -12,22 +12,12 @@ class help(commands.Cog):
 
     @commands.command(name='help')
     @commands.guild_only()
-    async def help(self, ctx):
-        try:
-            embed = discord.Embed(
-                description="Here are the commands!", color=0x00efdb)
-            embed.set_image(url="https://i.imgur.com/vMp7vWq.png")
-            await ctx.reply(embed=embed, mention_author=True)
-            await asyncio.sleep(1000)
-        except Exception as e:
-            embed = discord.Embed(title=":x: Command Error",
-            colour=0x992D22)  # Dark Red
-            embed.add_field(name="Error", value=e)
-            embed.add_field(name="Guild", value=ctx.guild)
-            embed.add_field(name="Channel", value=ctx.channel)
-            embed.add_field(name="User", value=ctx.author)
-            embed.timestamp = datetime.datetime.utcnow()
-            await ctx.reply(embed=embed)
+    async def help(self, interaction: discord.Interaction):
+        embed = discord.Embed(
+            description="Here are the commands!", color=0x00efdb)
+        embed.set_image(url="https://i.imgur.com/vMp7vWq.png")
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+        await asyncio.sleep(1000)
 
 def setup(bot):
     bot.add_cog(help(bot))
