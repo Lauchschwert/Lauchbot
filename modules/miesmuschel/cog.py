8866473ccb419a838b5c1251d6076a35b42adc85
Andreas <andi@anditv.dev>
2023-08-02 21:17:11 +0200

    Updated miesmuschel command
    NOT TESTED YET

--- modules/miesmuschel/cog.py
+++ modules/miesmuschel/cog.py
@@ -10,55 +10,45 @@ class miesmuschel(commands.Cog):
         self.bot = bot
 
     @commands.command(name='miesmuschel')
-    async def miesmuschel(self, ctx, question):
-        try:
-            answers = [
-                "Yes",
-                "yep",
-                "yea",
-                "yup"
-                "No",
-                'Positive',
-                'From my point of view, yes',
-                'From my point of view, no',
-                'Convinced.',
-                'Most Likley.',
-                'Chances High',
-                "of course not",
-                "negative",
-                "never",
-                "Ask me again",
-                "Why do you need to ask?",
-                "Go away. I do not wish to answer at this time.",
-                "Time will only tell",
-                "Maybe in an hour",
-                "perhaps",
-                "maybe",
-                "possibly",
-                "Maybe one day",
-                "I don't think so",
-                "I dont care",
-                "If it makes you happpier... Yes.",
-                "I dont think so.",
-                "How about...... No!",
-                "How about...... Yes!",
-                "Dont ask me that...",
-                'Not Sure',
-                'Maybe',
-                'I cannot predict now.',
-                'Im to lazy to predict.',
+    async def miesmuschel(self, interaction: discord.Interaction):
+        answers = [
+            "Yes",
+            "yep",
+            "yea",
+            "yup"
+            "No",
+            'Positive',
+            'From my point of view, yes',
+            'From my point of view, no',
+            'Convinced.',
+            'Most Likley.',
+            'Chances High',
+            "of course not",
+            "negative",
+            "never",
+            "Ask me again",
+            "Why do you need to ask?",
+            "Go away. I do not wish to answer at this time.",
+            "Time will only tell",
+            "Maybe in an hour",
+            "perhaps",
+            "maybe",
+            "possibly",
+            "Maybe one day",
+            "I don't think so",
+            "I dont care",
+            "If it makes you happpier... Yes.",
+            "I dont think so.",
+            "How about...... No!",
+            "How about...... Yes!",
+            "Dont ask me that...",
+            'Not Sure',
+            'Maybe',
+            'I cannot predict now.',
+            'Im to lazy to predict.',
 
-            ]
-            await ctx.send(random.choice(answers))
-        except Exception as e:
-            embed = discord.Embed(title=":x: Command Error",
-            colour=0x992D22)  # Dark Red
-            embed.add_field(name="Error", value=e)
-            embed.add_field(name="Guild", value=ctx.guild)
-            embed.add_field(name="Channel", value=ctx.channel)
-            embed.add_field(name="User", value=ctx.author)
-            embed.timestamp = datetime.datetime.utcnow()
-            await ctx.reply(embed=embed)
+        ]
+        await interaction.response.send_message(random.choice(answers))
 
 def setup(bot):
     bot.add_cog(miesmuschel(bot))
