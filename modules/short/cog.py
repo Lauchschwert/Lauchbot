66ac7fb35873effe20e3790ffd2604460224b2a7
Andreas <andi@anditv.dev>
2023-08-02 21:22:43 +0200

    Updated short command

--- modules/short/cog.py
+++ modules/short/cog.py
@@ -13,22 +13,13 @@ class short(commands.Cog):
         self.bot = bot
 
     @commands.command(name='short')
-    async def short(self, ctx, url):
-        try:
-            link = "http://tinyurl.com/api-create.php?url=" + str(url)
-            from urllib.request import urlopen
-            with urlopen(link) as webpage:
-                f = webpage.read().decode()
-            await ctx.send(f"Your short-link is: {f}")
-        except Exception as e:
-            embed = discord.Embed(title=":x: Command Error",
-            colour=0x992D22)  # Dark Red
-            embed.add_field(name="Error", value=e)
-            embed.add_field(name="Guild", value=ctx.guild)
-            embed.add_field(name="Channel", value=ctx.channel)
-            embed.add_field(name="User", value=ctx.author)
-            embed.timestamp = datetime.datetime.utcnow()
-            await ctx.reply(embed=embed)
+    async def short(self, interaction: discord.Interaction, url):
+        link = "http://tinyurl.com/api-create.php?url=" + str(url)
+        from urllib.request import urlopen
+        with urlopen(link) as webpage:
+            f = webpage.read().decode()
+        await interaction.response.send_message(f"Your short-link is: {f}")
+
 
 def setup(bot):
     bot.add_cog(short(bot))
