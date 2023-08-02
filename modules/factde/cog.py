4ceb63f0848f3166f00997a6e46785f140f8b2d9
Andreas <andi@anditv.dev>
2023-08-02 21:11:17 +0200

    Updated factde command
    NOT TESTED YET

--- modules/factde/cog.py
+++ modules/factde/cog.py
@@ -10,21 +10,21 @@ class factde(commands.Cog):
         self.bot = bot
 
     @commands.command(name='factde')
-    async def factde(self, ctx):
+    async def factde(self, interaction: discord.Interaction):
         try:
             url = "https://uselessfacts.jsph.pl/random.json?language=de"
             response = requests.get(url)
             value = response.json()["text"]
-            await ctx.send("Hier ist eine zufälliger und nutzloser Fakt: " + value)
+            await interaction.send("Hier ist eine zufälliger und nutzloser Fakt: " + value)
         except Exception as e:
             embed = discord.Embed(title=":x: Command Error",
             colour=0x992D22)  # Dark Red
             embed.add_field(name="Error", value=e)
-            embed.add_field(name="Guild", value=ctx.guild)
-            embed.add_field(name="Channel", value=ctx.channel)
-            embed.add_field(name="User", value=ctx.author)
+            embed.add_field(name="Guild", value=interaction.guild)
+            embed.add_field(name="Channel", value=interaction.channel)
+            embed.add_field(name="User", value=interaction.author)
             embed.timestamp = datetime.datetime.utcnow()
-            await ctx.reply(embed=embed)
+            await interaction.reply(embed=embed)
 
 def setup(bot):
     bot.add_cog(factde(bot))
