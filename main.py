179b1831848d8239b82257029ede04ffb1a62626
Andreas <andi@anditv.dev>
2023-08-02 19:45:14 +0200

    Updated loading functions

--- main.py
+++ main.py
@@ -2,95 +2,79 @@ import os
 import asyncio
 import discord
 import datetime
-from discord import guild
-from discord.ext import commands
-from discord.ext.commands import has_permissions
-from discord.utils import get
 from discord.ext import commands
 import requests
 import json
 import sys
 
-CLIENT_ID = 't6b4d5a942qvmgpw0t0cb3a3r8jc6g'
-AUTH_TOKEN = 'hbma7xqs6m75iq3qinu1anx9exg1cy'
-
-STREAMER_NAME = 'Lauchschwert'
-
-CHANNEL_ID = '863582397944692756'
-
-client = discord.Client()
-
-async def send_discord_message(message):
-    channel = client.get_channel(int(863582397944692756))
-    await channel.send(message)
-
-def check_stream_status():
-    url = f'https://api.twitch.tv/helix/streams?user_login=lauchschwert'
-    headers = {'Client-ID': f'Bearer t6b4d5a942qvmgpw0t0cb3a3r8jc6g', 'Authorization': f'Bearer hbma7xqs6m75iq3qinu1anx9exg1cy'}
-    response = requests.get(url, headers=headers)
-    data = json.loads(response.text)
-    if data['data']:
-        return True
-    else:
-        return False
-
-async def send_live_notification():
-    message = f'Lauchschwert just went live on Twitch! Watch the stream here: https://www.twitch.tv/lauchschwert'
-    await send_discord_message(message)
-
-async def on_ready():
-    while True:
-        if check_stream_status():
-            await send_live_notification()
-        await asyncio.sleep(60)
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
 
 def main():
-    client = commands.Bot(
+    bot = commands.Bot(
         command_prefix=commands.when_mentioned_or(">>>"),
+        intents=intents
     )
-    client.remove_command("help")
+    bot.remove_command("help")
 
-    @client.event
+    @bot.event
     async def on_ready():
-        print(f"{client.user.name} has connected to Discord.")
-        await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name="Jojo's bizarre adventure"))
+        print(f"{bot.user.name} has connected to Discord.")
+        activity = discord.Activity(type=discord.ActivityType.watching, name="Jojo's bizarre adventure")
+        await bot.change_presence(status=discord.Status.dnd, activity=activity)
 
-    @client.event
+    @bot.event
     async def on_message(message):
-        if message.author == client.user:
-            return
+        if message.author.bot:
+            return False
+        await bot.process_commands(message)
 
-        await client.process_commands(message)
-
-    @client.event
+    @bot.event
     async def on_command_error(ctx, error):
-        embed = discord.Embed(title=":x: Command Error",
-                              colour=0x992D22)  # Dark Red
-        embed.add_field(name="Error", value=error)
-        embed.add_field(name="Guild", value=ctx.guild)
-        embed.add_field(name="Channel", value=ctx.channel)
-        embed.add_field(name="User", value=ctx.author)
+        embed = discord.Embed(title=":x: Command Error", colour=0x992D22)  # Dark Red
+        embed.add_field(name="Error", value=str(error))
+        embed.add_field(name="Guild", value=ctx.guild.name)
+        embed.add_field(name="Channel", value=ctx.channel.name)
+        embed.add_field(name="User", value=ctx.author.name)
         embed.add_field(name="Message", value=ctx.message.clean_content)
         embed.timestamp = datetime.datetime.utcnow()
         try:
             await ctx.reply(embed=embed)
+        except discord.Forbidden:
+            await ctx.send("An error occurred, but I couldn't send the error message. Please check my permissions.")
         except:
             pass
 
-    # load all cogs
-    for folder in os.listdir("modules"):
-        if os.path.exists(os.path.join("modules", folder, "cog.py")):
-            client.load_extension(f"modules.{folder}.cog")
+    # load all modules
+    class Bot(commands.Bot):
+        def __init__(self, *, intents: discord.Intents):
+
+            super().__init__(command_prefix=commands.when_mentioned_or("$$"), intents=intents)
+
+        async def setup_hook(self):
+            for filepath in os.listdir('modules'):
+                for filename in os.listdir(f'modules/{filepath}'):
+                    if filename.endswith('.py'):
+                        filename = filename.replace('.py', '')
+                        allmodules += 1
+                        try:
+                            await bot.load_extension(f'modules.{filepath}.{filename}')
+                            print(f'Loaded modules.{filepath}.{filename}')
+                            loaded += 1
+                        except Exception as error:
+                            print(f'Failed to load modules.{filepath}.{filename}: {error}')
+
+            await self.tree.sync()
 
     with open("config.json", "r", encoding="UTF-8") as configfile:
         config = json.load(configfile)
         token = config.get("token")
         if not token:
-            print(f"[ERROR] Value for key 'token' is missing in config.json. Please check the configuration file and try again.")
+            print("[ERROR] Value for key 'token' is missing in config.json. Please check the configuration file and try again.")
             sys.exit()
-    configfile.close()
-    client.run(token)
-
+    
+    bot.run(token)
 
 if __name__ == '__main__':
     main()
