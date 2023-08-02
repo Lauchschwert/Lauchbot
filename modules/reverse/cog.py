7b0f445ee5bc794fc9a94f41811d670d3d72d358
Andreas <andi@anditv.dev>
2023-08-02 21:19:41 +0200

    Updated reverse command

--- modules/reverse/cog.py
+++ modules/reverse/cog.py
@@ -12,18 +12,8 @@ class reverse(commands.Cog):
         self.bot = bot
 
     @commands.command(name='reverse')
-    async def reverse(self, ctx, text):
-        try:
-            await ctx.send(text[::-1])
-        except Exception as e:
-            embed = discord.Embed(title=":x: Command Error",
-            colour=0x992D22)  # Dark Red
-            embed.add_field(name="Error", value=e)
-            embed.add_field(name="Guild", value=ctx.guild)
-            embed.add_field(name="Channel", value=ctx.channel)
-            embed.add_field(name="User", value=ctx.author)
-            embed.timestamp = datetime.datetime.utcnow()
-            await ctx.reply(embed=embed)
+    async def reverse(self, interaction: discord.Interaction, text):
+        await interaction.response.send_message(text[::-1])
 
 def setup(bot):
     bot.add_cog(reverse(bot))
